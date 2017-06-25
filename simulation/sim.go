2ca655ff142e44e96a41f7607a8d3d6cad4cf81c
Noofbiz <caligiure.ja@gmail.com>
2017-06-25 10:39:49 -0700

    simulation results
    now returns results

--- simulation/sim.go
+++ simulation/sim.go
@@ -2,7 +2,13 @@ package simulation
 
 import "fmt"
 
+// Results holds the data obtained from the simulation
+type Results struct {
+	Susceptible, Infected, Resistant int
+}
+
 // Simulate runs the actual simulation for the program to display
-func Simulate(pop, initI int, S2I, I2R, S2R float64) {
+func Simulate(pop, initI int, S2I, I2R, S2R float64) []Results {
 	fmt.Println("hi")
+	return []Results{{0, 0, 0}, {1, 1, 1}}
 }
